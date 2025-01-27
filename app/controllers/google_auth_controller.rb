e65b9b6c1289ee50ff2f0189c3491cb2875ab90a
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-01-27 16:30:18 +0900

    OAuth認証の追加

---
+++ app/controllers/google_auth_controller.rb
@@ -0,0 +1,32 @@
+class GoogleAuthController < ApplicationController
+  GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
+  def get_access_token
+    code = params[:code]
+
+    response = HTTParty.post(GOOGLE_TOKEN_URL, {
+      headers: { "Content-Type" => "application/x-www-form-urlencoded" },
+      body: {
+        code: code,
+        client_id: "#{ENV['CLIENT_ID']}",
+        client_secret: "#{ENV['CLIENT_SECRET']}",
+        redirect_uri: "#{ENV['REDIRECT_URL']}",
+        grant_type: "authorization_code"
+      }
+    })
+
+    if response.code == 200
+      # アクセストークンを取得
+      @access_token = response["access_token"]
+      refresh_token = response["refresh_token"]
+
+      render json: {
+        access_token: @access_token,
+        refresh_token: refresh_token,
+        expires_in: response["expires_in"]
+      }
+    else
+      render json: { error: response["error"], description: response["error_description"] }, status: :bad_request
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # get "homes/top"
-
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  Rails.application.routes.draw do
+    get "auth/google_oauth2/callback", to: "google_auth#get_access_token"
+  end
 
   root "homes#top"
 end
